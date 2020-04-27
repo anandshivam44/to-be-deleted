b64e299a56c26df22a5235fc9d7daf732daee932
Shivam Anand <anand.shivam44@yahoo.com>
2020-04-27 17:05:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/testingproject/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.testingproject;
+import android.Manifest;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import com.hololo.tutorial.library.PermissionStep;
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+
+public class MainActivity extends TutorialActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addFragment(
+                new Step.Builder()
+                        .setTitle(getString(R.string.page_0_title))
+                        .setContent(getString(R.string.page_0_content))
+                        .setBackgroundColor(Color.parseColor("#FF0957"))//  #CA70F3
+                        .setDrawable(R.drawable.fig_del)
+                        .setSummary(getString(R.string.page_0_summary))
+                        .build());
+        addFragment(
+                new Step.Builder()
+                        .setTitle(getString(R.string.page_1_title))
+                        .setContent(getString(R.string.page_1_content))
+                        .setBackgroundColor(Color.parseColor("#99FF0957"))
+                        .setDrawable(R.drawable.fig_1)
+                        .setSummary(getString(R.string.page_1_summary))
+                        .build());
+        addFragment(
+                new Step.Builder()
+                        .setTitle(getString(R.string.page_2_title))
+                        .setContent(getString(R.string.page_2_content))
+                        .setBackgroundColor(Color.parseColor("#00D4BA"))
+                        .setDrawable(R.drawable.fig_2)
+                        .setSummary(getString(R.string.page_2_summary))
+                        .build());
+        addFragment(
+                new Step.Builder()
+                        .setTitle(getString(R.string.page_3_title))
+                        .setContent(getString(R.string.page_3_content))
+                        .setBackgroundColor(Color.parseColor("#1098FE"))
+                        .setDrawable(R.drawable.fig_3)
+                        .setSummary(getString(R.string.page_3_summary))
+                        .build());
+
+        addFragment(
+                new PermissionStep
+                        .Builder()
+                        .setPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_SMS,Manifest.permission.ACCESS_FINE_LOCATION})
+                        .setTitle(getString(R.string.page_4_title)).setContent(getString(R.string.page_4_content))
+                        .setBackgroundColor(Color.parseColor("#8BCEAB"))//#8BCEAB
+                        .setDrawable(R.drawable.fig_4)
+                        .setSummary(getString(R.string.page_4_summary))
+                        .build());
+    }
+
+    @Override
+    public void finishTutorial() {
+        Toast.makeText(this, "Tutorial finished", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    public void currentFragmentPosition(int position) {
+        Toast.makeText(this, "Position : " + position, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/testingproject/NavigationDrawer.java
@@ -0,0 +1,104 @@
+package com.example.testingproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class NavigationDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_Message()).commit();
+            navigationView.setCheckedItem(R.id.nav_message);
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_Message()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_Chat()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_Profile()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "anand.shivam44@yahoo.com", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new Fragment_Chat();
+                            break;
+                        case R.id.nav_favorites:
+                            selectedFragment = new Fragment_Profile();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new Fragment_Message();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
